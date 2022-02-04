f30db91c88820df1ce3fa617ec46178799ea2d89
Monoget <monoget2@gmail.com>
2022-02-04 21:55:14 +0600

    Initial Push

---
+++ src/contracts/KryptoBirdz.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./ERC721Connector.sol";
+
+contract Kryptobird is ERC721Connector{
+
+    constructor() ERC721Connector('KryptoBird','KBIRD'){
+
+    }
+}
