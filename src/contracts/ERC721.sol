3993954f02187e2fd9a40478400158eb6741face
Monoget <monoget2@gmail.com>
2022-02-06 00:00:34 +0600

    done #13 lesson

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -2,7 +2,49 @@
 pragma solidity ^0.8.0;
 
 contract ERC721 {
-    function ERC721(){
 
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId
+    );
+
+    mapping(uint => address) private _tokenOwner;
+
+    mapping(address => uint256) private _OwnedTokensCount;
+
+    function balanceOf(address _owner) public view returns(uint256){
+        require(_owner != address(0), 'Owner query for non-existent zero.');
+
+        return _OwnedTokensCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) public view returns(address){
+
+        address owner=_tokenOwner[_tokenId];
+
+        require(owner != address(0), 'Owner query for non-existent zero.');
+
+        return owner;
+    }
+
+
+    function _exists(uint256 tokenId) internal view returns (bool){
+        address owner = _tokenOwner[tokenId];
+
+        return owner != address(0);
+    }
+
+
+    function _mint(address to, uint256 tokenId) internal virtual {
+        require(to != address(0), 'ERC721: Minting to the zero address');
+
+        require(!_exists(tokenId), 'ERC721: token already minted');
+
+        _tokenOwner[tokenId] = to;
+
+        _OwnedTokensCount[to] += 1;
+
+        emit Transfer(address(0), to, tokenId);
     }
 }
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -3,9 +3,25 @@ pragma solidity ^0.8.4;
 
 import "./ERC721Connector.sol";
 
-contract Kryptobird is ERC721Connector{
+contract KryptoBird is ERC721Connector {
 
-    constructor() ERC721Connector('KryptoBird','KBIRD'){
+    string [] public KryptoBirdz;
+
+    mapping(string => bool) _kryptoBirdzExists;
+
+    function mint(string memory _kryptoBird) public {
+
+        require(!_kryptoBirdzExists[_kryptoBird], 'Error: KryptoBird already exists');
+
+        KryptoBirdz.push(_kryptoBird);
+        uint _id = KryptoBirdz.length - 1;
+        _mint(msg.sender, _id);
+
+        _kryptoBirdzExists[_kryptoBird] = true;
+    }
+
+
+    constructor() ERC721Connector('KryptoBird', 'KBIRD'){
 
     }
 }
